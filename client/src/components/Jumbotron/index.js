7791f407e394edff3db835e6447326c25251b65f
Melody Monarch <oliva.sam.so@gmail.com>
2021-04-01 18:01:02 -0500

    bootstrap jumbotron added

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../../assets/vision-logo.ico"
+
+
+export function Jumbotron() {
+    return (
+        <div className="jumbotron">
+            <img src={logo} alt="vision-logo"></img>
+        </div>
+    );
+}
+
