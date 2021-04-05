ba21914cf91c4e430af1f39abf0b193b28c03d76
Melody Monarch <oliva.sam.so@gmail.com>
2021-04-05 11:11:27 -0500

    added react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Login } from "./pages/login";
+import { UserBoard } from "./pages/userBoard";
 
 
 function App() {
   return (
-    <Login></Login>
+    <Router>
+
+      <Switch>
+        <Route exact path="/">
+          <Login></Login>
+        </Route>
+
+        <Route exact path="/user">
+          <UserBoard></UserBoard>
+        </Route>
+      </Switch>
+
+    </Router>
+
   );
 }
 
