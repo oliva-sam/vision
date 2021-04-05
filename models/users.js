92e85484983b13b58467c82fd464e06b3315ab9c
Melody Monarch <oliva.sam.so@gmail.com>
2021-04-05 11:09:40 -0500

    created user login schema

---
+++ models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            required: "Please enter a username"
+        },
+        password:{
+            type: String,
+            required: "Please enter a password"
+        }
+    }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
