005cc7e15fa39bd8e5993969a7dc8ccff7a1118c
Melody Monarch <oliva.sam.so@gmail.com>
2021-04-06 17:18:54 -0500

    passport and express router stuff

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+
+router.post("/login", (req, res, next) => {
+    passport.authenticate("local", function(err, user, info) {
+        if(err) {
+            return res.status(400).json({errors:err});
+        }
+        if (!user) {
+            return res.status(400).json({errors: "no user found"});
+        }
+        req.logIn(user, function(err) {
+            if(err) {
+                return res.status(400).json({errors:err});
+            }
+            return res.status(200).json({success:`logged in ${user.id}`});
+        });
+    })(res, res, next);
+});
+
+module.exports = router;
