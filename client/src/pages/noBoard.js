ea1e6434dadbcf75ea4a78293ff38c01e342238c
Melody Monarch <oliva.sam.so@gmail.com>
2021-04-06 22:08:13 -0500

    messed with front-end to see how layout will go

--- client/src/pages/noBoard.js
+++ client/src/pages/noBoard.js
@@ -1,7 +1,49 @@
 import React from "react";
 import { Container, Row, Col } from "../components/Grid";
+import Modal from "@material-ui/core/Modal";
+import { FormBtn, Input } from "../components/Form";
+
 
 export function NoBoard () {
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleBugSubmit = event => {
+        event.preventDefault();
+        console.log("form submitted");
+    }
+
+    const bugBody= (
+        <>
+         <h3 id="simple-modal-title">Thank you for your feedback!</h3>
+        < form onSubmit={handleBugSubmit}> 
+        <Input 
+            type="text"
+            name="Location"
+            placeholder="Location of Bug"
+        />
+
+        <Input 
+            type="text"
+            name="Description"
+            placeholder="Description of Bug"
+        />
+
+        <FormBtn>
+            <p>send</p>
+        </FormBtn>
+
+        </form>
+        </>
+    );
+
     return (
         <>
         <Container className="fluid">
@@ -23,7 +65,16 @@ export function NoBoard () {
                     <button 
                     type="button" 
                     className="btn btn-warning"
+                    onClick={handleOpen}
                     >report bug</button>
+
+                    <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="simple-modal-title"
+                    >
+                        {bugBody}
+                    </Modal>
                 </Col>
                 <Col size="md-4"></Col>
             </Row>
--- client/src/pages/userBoard.js
+++ client/src/pages/userBoard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Jumbotron} from "../components/Jumbotron";
 import { Container, Row, Col } from "../components/Grid";
+import { NewGoal } from "../components/NewGoal";
 
 export function UserBoard () {
     return (
@@ -10,6 +11,11 @@ export function UserBoard () {
             <Row>
                 <Col size="md-12">
                 <h1>Hello</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col size="md-12">
+                < NewGoal></NewGoal>
                 </Col>
             </Row>
         </Container>
