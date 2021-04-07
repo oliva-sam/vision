1e105f8ee36580e538bdaa2416b1efea3e0c2f60
Melody Monarch <oliva.sam.so@gmail.com>
2021-04-06 22:07:10 -0500

    adding a new goal feature template

---
+++ client/src/components/NewGoal/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box } from "rebass";
+
+export function NewGoal () {
+    return (
+        < Box 
+            p={20}
+            fontSize={20}
+            width={[ 1, 1, 1 ]}
+            color="white"
+            bg="gray">
+            +
+        </Box>
+    )
+}
