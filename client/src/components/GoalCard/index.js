186c4d27d09682c113e696013fde6c489fde2d68
Melody Monarch <oliva.sam.so@gmail.com>
2021-04-07 22:35:11 -0500

    created bootstrap goal cards component

---
+++ client/src/components/GoalCard/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+import PlaceholderImg from "../../assets/placeholder-300.png";
+
+export function GoalCard(props) {
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img alt={props.name} src={PlaceholderImg}/>
+            </div>
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Category:</strong> {props.category}
+                    </li>
+                </ul>
+            </div>
+            
+        </div>
+    )
+}
