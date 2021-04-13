1949ec31016d80ab2a034d3721264c7947da152e
Melody Monarch <oliva.sam.so@gmail.com>
2021-04-13 15:08:08 -0500

    heroku stuff missing so added

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true}
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
 
 // Database Connection
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/visionDB",
